897a979b0f64a4d1aca012c2b8eadd3b6a15f739
brook_lan <brook.lan@go-capture.com>
2018-03-13 13:29:27 +0800

    增加微博内容爬虫

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from utils import AuthorPipeline
 from spider import AuthorSpider
+from spider.weibo import WeiboSpider
 from login import WeiboLogin
 
-
-if __name__ == "__main__":
+def crawl_author():
     mongo_url = "localhost"
     mongo_db = "weibo"
     mongo_coll = "author"
@@ -15,5 +15,28 @@ if __name__ == "__main__":
     with AuthorPipeline(mongo_url, mongo_db, mongo_coll) as pipe:
         for item in sp.crawl():
             pipe.save(item)
+            
+            
+def crawl_weibo():
+    account = input("enter your weibo account:\n")
+    pwd = input("enter your passwords:\n")
+    lg = WeiboLogin(account, pwd)
+    sp = WeiboSpider(lg)
+    
+    id_ = "1006062557129567"
+    
+    from utils import MongoPipeline
+    mongo_url = "192.168.2.126"
+    mongo_db = "test"
+    mongo_coll = "weibo_new"
+    
+    with MongoPipeline(mongo_url, mongo_db, mongo_coll) as pipe:
+        for item in sp.crawl(id_):
+            pipe.save(item)    
+
+
+if __name__ == "__main__":
+    crawl_weibo()
+
 
 
---
+++ spider/weibo.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@create Time:2018-03-13
+
+@author:Brook
+"""
+import re
+import time
+
+from bs4 import BeautifulSoup
+
+
+from spider.base import Spider
+from login import WeiboLogin
+
+
+class WeiboUrl:
+    """
+    待爬取的url由url模板生成
+    """
+    def __init__(self):
+        self.tmplate = "https://weibo.com/p/aj/v6/mblog/mbloglist?ajwvr=6&domain={domain}&is_search=0&visible=0&is_all=1&is_tag=0&profile_ftype=1&page={page}&pagebar={pagebar}&id={id}"
+        
+    def pagenums_url_of_id(self, id_):
+        """
+        """
+        para = {"page":1,
+                "pagebar":1,
+                "id": id_,
+                "domain": id_[:6]
+        }
+        url = self.tmplate.format_map(para) + "&pre_page=1"
+        return url
+    
+    def contentpage_url_of_id(self, id_, page_nums):
+        for page in range(1, page_nums+1):
+            url = self.tmplate.format(page=page, pagebar=0, id=id_, domain=id_[:6])
+            yield url
+            for j in range(0, 2):
+                url = self.tmplate.format(page=page, pagebar=j, id=id_, domain=id_[:6]) + "&pre_page=%s"%page
+                yield url
+                
+
+class WeiboSpider(Spider):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.urlgen = WeiboUrl()
+        
+    def get_page_nums(self, id_):
+        url = self.urlgen.pagenums_url_of_id(id_)
+        d = self.get_json(url)['data']
+        reg = "countPage=(\d+)"
+        try:
+            page_num = int(re.findall(reg, d, re.S)[0])
+        except:
+            page_num = 0
+        return page_num
+    
+    def get_urls(self, id_):
+        page_nums = self.get_page_nums(id_)
+        for url in self.urlgen.contentpage_url_of_id(id_, page_nums):
+            yield url
+            
+    def parse(self, json_data):
+        text = json_data['data']
+        soup = BeautifulSoup("<html><head></head><body>" + text + "</body></html>", "lxml")
+        reg = '<em>(\d+)</em>'
+        tmp = soup.find_all("div", attrs={"class": "WB_detail"})
+        tmp2 = soup.find_all("div", attrs={"class":"WB_handle"})
+        if len(tmp) > 0 :
+            for i in range(len(tmp)):
+                item = {}
+                item["nickname"] = tmp[i].find("div", attrs={"class": "WB_info"}).find("a").get_text()
+                item["Post"] = tmp[i].find("div", attrs={"class": "WB_text W_f14"}).get_text().replace("\n", "").replace(" ","").replace( "\u200b", "")
+
+                # -*- 爬取发布时间 -*-
+                item["Pubtime"] = tmp[i].find("a", attrs={"class": "S_txt2"}).get("title")
+
+                # -*- 爬取转发数 -*-
+                if re.findall(reg,str(tmp2[i].find("span", attrs={"class": "line S_line1","node-type":"forward_btn_text"})), re.S):
+                    item["Transfer_num"] = int(re.findall(reg,str(tmp2[i].find("span", attrs={"class": "line S_line1","node-type":"forward_btn_text"})), re.S)[0])
+                else:
+                    item["Transfer_num"] = 0
+
+                # -*- 爬取评论数 -*-
+                if re.findall(reg, str(tmp2[i].find("span", attrs={"class": "line S_line1", "node-type": "comment_btn_text"})), re.S):
+                    item["Comment_num"] = int(re.findall(reg, str(tmp2[i].find("span", attrs={"class": "line S_line1", "node-type": "comment_btn_text"})), re.S)[0])
+                else:
+                    item["Comment_num"] = 0
+
+                # -*- 爬取点赞数 -*-
+                if re.findall(reg, str(tmp2[i].find("span", attrs={"node-type": "like_status"})), re.S):
+                    item["Like_num"] = int(re.findall(reg, str(tmp2[i].find("span", attrs={"node-type": "like_status"})), re.S)[0])
+                else:
+                    item["Like_num"] = 0
+                item["Scraltime"] = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                yield item
+                
+    def crawl(self, id_):
+        for url in self.get_urls(id_):
+            print(url)
+            d = self.get_json(url)
+            for item in self.parse(d):
+                yield item
+            
+
+
+
+
+
+
+
+
+
+    
